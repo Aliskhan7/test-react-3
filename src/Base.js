7d7dad2609f0ee3cac85fab7196bca9f2c9a3344
juleybib <aliskhangazamatov@gmail.com>
2021-04-05 13:26:02 +0300

    first

---
+++ src/Base.js
@@ -0,0 +1,13 @@
+
+import logo from './logo512.png'
+
+function Base(){
+    return(
+        <div>
+            <h1>Helloy</h1>
+            <img src={logo} />
+        </div>
+    )
+
+}
+export default Base;
